7de7b36a814d34fd86851f033fbaf452a27c68bc
Abhimanyu Borthakur <abhimanyuborthakur@hotmail.com>
2022-01-25 18:16:06 +0530

    updated codes

--- Day 2/Assignments(Challenges)/Question1_StackQuery.py
+++ Day 2/Assignments(Challenges)/Question1_StackQuery.py
@@ -19,32 +19,53 @@ class Stack:
     def max(self):
         return max(self.stack)
 
+#driver code
 if __name__ == '__main__':
+    flag_typ = True
+    flag_ele =  True
     print("Enter input in specified format:")
     n = (int)(input())
-    ops = []
-    for i in range(n):
-        ops.append(input())
+
+    #constraint 1
+    if n>=1 and n<=10**5:
+        ops = []
+        for i in range(n):
+            temp = input()
+            ops.append(temp)
+            temp_arr = list(map(int,temp.split()))
+            #constraint 2
+            if not(temp_arr[0]>=1 and temp_arr[0]<=3):
+                flag_type = False
+                print("Invalid type entered, start over")
+                break
+            #constraint 3
+            if not(temp_arr[1]>=1 and temp_arr[1]<=10**9):
+                flag_ele = False
+                print("Invalid element, start over")
+                break
     
     
-    def getMax(operations = None):
+        def getMax(operations = None):
 
-        myStack = Stack()
-        ele = []
+            myStack = Stack()
+            ele = []
 
-        for op in operations:
-            op_arr = list(map(int,op.split()))
-            if len(op_arr)==2:
-                myStack.push(op_arr[1])
-            elif op_arr[0] == 2:
-                c= myStack.pop()
-            else:
-                ele.append(myStack.max())
+            for op in operations:
+                op_arr = list(map(int,op.split()))
+                if len(op_arr)==2:
+                    myStack.push(op_arr[1])
+                elif op_arr[0] == 2:
+                    c= myStack.pop()
+                else:
+                    ele.append(myStack.max())
         
-        return ele
+            return ele
 
-    ele = getMax(operations = ops)
-    for el in ele:
-        print(el)
+        if flag_typ and flag_ele:
+            ele = getMax(operations = ops)
+            for el in ele:
+                print(el)
+    else:
+        print("Invalid no of queries, start over")
 
 
